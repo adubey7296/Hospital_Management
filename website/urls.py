8cee310ddb2b7ce8754f88d90d61e168013fa752
adubey7296 <adubey7296@gmail.com>
2020-08-29 18:31:30 +0530

    project initialization

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('', include(('home.urls', 'home'), namespace='home')),
+    path('admin/', admin.site.urls),
+    path('index/', include('home.urls')),
+]
